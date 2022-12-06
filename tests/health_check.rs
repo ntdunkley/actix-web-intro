6ec04dec66ace4b8d99795526deef306e410dadc
ndunkley <nathan.dunkley@bondsmith.co.uk>
2022-12-06 23:19:04 +0000

    rebase

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,6 +16,52 @@ async fn health_check_works() {
     assert_eq!(response.content_length(), Some(0));
 }
 
+#[tokio::test]
+async fn when_subscribe_with_valid_form_data_return_200() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "_name=bryan&_email=bryan%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute POST subscribe");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn when_subscribe_with_invalid_form_data_return_400() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=bryan", "missing email"),
+        ("email=bryan@gmail.com", "missing name"),
+        ("", "missing name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute POST subscribe");
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "The API did not return 400 when payload was {}",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
