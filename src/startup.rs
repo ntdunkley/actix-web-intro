6a95b38c7750d35afc6b2ccb8d3992c438107951
ndunkley <nathan.dunkley@bondsmith.co.uk>
2023-06-19 22:18:16 +0100

    Write test for not sending emails to unconfirmed subscribers

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::config::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{confirm, health_check, subscribe};
+use crate::routes::{confirm, health_check, publish_newsletter, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -67,6 +67,7 @@ pub fn run(
         App::new()
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
+            .route("/newsletters", web::post().to(publish_newsletter))
             .route("/subscriptions", web::post().to(subscribe))
             .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone())
